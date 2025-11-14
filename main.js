d5214f88f62673c88fec90811cf74910638d3a85
hacc-ount <214715279+hacc-ount@users.noreply.github.com>
2025-11-14 04:40:57 +0000

    Change the script type to module and restructure the javascript code for allowing expansion

--- main.js
+++ main.js
@@ -1,73 +1,12 @@
-const display = document.getElementById("display");
-let timer = null; //timer will hold value of the timer as it plays, stops. Seperate to the Date() object data.
-let startTime = 0;
-let elapsedTime = 0;
-let isRunning = false; //if the stopwatch is running, flip this to be true, if not then it will be false.
+import {start, pause} from "./src/display.js";
 
-const log = document.getElementById("log");
-let pauseData = "";
-let notesData = "";
-let finishData = "";
+const startBtn = document.querySelector("#startBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
 
-function start() {
+startBtn.addEventListener("click", () => {
+    start();
+});
 
-    if (!isRunning) {
-        //if not running, start the stopwatch
-        startTime = Date.now() - elapsedTime;
-        timer = setInterval(update, 10);
-        display.classList.remove("animatePause");
-        display.classList.add("animateStart");
-        isRunning = true;
-    }
-
-}
-
-function pause() {
-    
-    if (isRunning) {
-        clearInterval(timer);
-        elapsedTime = Date.now() - startTime;
-
-        display.classList.remove("animateStart");
-        display.classList.add("animatePause");
-
-        /*pauseData = window.prompt("Please list reason for pausing:");
-
-        let worldTime = new Date().toLocaleString();
-        let tempP = document.createElement("p");
-        tempP.textContent = `${worldTime} - Pause reason: ${pauseData}`;
-        log.appendChild(tempP);*/
-
-        isRunning = false;
-    }
-
-}
-
-function reset() {
-    clearInterval(timer);
-    startTime = 0;
-    elapsedTime = 0;
-    isRunning = false;
-    display.textContent = "00:00:00"
-}
-
-function update() {
-
-    const currentTime = Date.now();
-    elapsedTime = currentTime - startTime;
-
-    let timerHours = Math.floor(elapsedTime / (1000 * 60 * 60));
-    let timerMinutes = Math.floor(elapsedTime / (1000 * 60) % 60);
-    let timerSeconds = Math.floor(elapsedTime / 1000 % 60);
-    let timerMilliseconds = Math.floor(elapsedTime % 1000 / 10);
-
-    timerHours = String(timerHours).padStart(2, "0");
-    timerMinutes = String(timerMinutes).padStart(2, "0");
-    timerSeconds = String(timerSeconds).padStart(2, "0");
-    timerMilliseconds = String(timerMilliseconds).padStart(2, "0");
-
-    display.textContent = `${timerHours}:${timerMinutes}:${timerSeconds}`;
-
-}
-
-
+pauseBtn.addEventListener("click", () => {
+    pause();
+});
---
+++ src/display.js
@@ -0,0 +1,50 @@
+const display = document.querySelector("#display");
+let timer = null 
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+
+function start() {
+
+    if (!isRunning) {
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(update, 10);
+        display.classList.remove("animatePause"); //Removing CSS animation
+        display.classList.add("animateStart"); //Replacing CSS animation
+        isRunning = true;
+    }
+
+}
+
+function pause() {
+
+    if (isRunning) {
+        clearInterval(timer);
+        elapsedTime = Date.now() - startTime;
+        display.classList.remove("animateStart"); //Removing CSS animation
+        display.classList.add("animatePause"); //Replacing CSS animation
+        isRunning = false;
+    }
+
+}
+
+function update() {
+
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+
+    let timerHours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let timerMinutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let timerSeconds = Math.floor(elapsedTime / 1000 % 60);
+    let timerMilliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+    timerHours = String(timerHours).padStart(2, "0");
+    timerMinutes = String(timerMinutes).padStart(2, "0");
+    timerSeconds = String(timerSeconds).padStart(2, "0");
+    timerMilliseconds = String(timerMilliseconds).padStart(2, "0");
+
+    display.textContent = `${timerHours}:${timerMinutes}:${timerSeconds}`;
+
+}
+
+export {start, pause};
